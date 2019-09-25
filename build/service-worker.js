55d715d61933c14d81c433a0127c7ce6134468a6
Megadurgurfishbait <gummehh@gmail.com>
2019-09-25 11:39:37 +0000

    laga myndir

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e548494619310f04f959310bbbff75c.js"
+  "/precache-manifest.8c613ef9998dffeb6f68ffa7bcfe585e.js"
 );
 
 workbox.clientsClaim();
